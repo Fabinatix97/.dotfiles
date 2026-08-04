818a774ce01ea8b26b5bf4a316776c1ce63774ca
Fabinatix97 <fabian@fabi-online.de>
2026-08-04 18:45:53 +0200

    Hide . and .. in netrw list

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,6 +5,7 @@ vim.g.have_nerd_font = true
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+vim.g.netrw_list_hide = [[^\.\.\=/\=$]]
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
