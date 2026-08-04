fa858f9381a5e14135eed5a41128c4968900e420
Fabinatix97 <fabian@fabi-online.de>
2026-08-04 18:49:12 +0200

    Keep cwd in sync with browsing directory for better telescope integration

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,6 +6,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_list_hide = [[^\.\.\=/\=$]]
+vim.g.netrw_keepdir = 0
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
