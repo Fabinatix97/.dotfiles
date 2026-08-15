24ca7d091d84ea33b1932c8b203b2fbd2c03cb70
Fabinatix97 <fabian@fabi-online.de>
2026-08-15 20:50:43 +0200

    Add hyprpaper

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -1,7 +1,6 @@
 -- https://wiki.hypr.land/Configuring/Basics/Autostart/
 hl.on("hyprland.start", function () 
-  hl.exec_cmd("waybar & swaync & hypridle")
-  hl.exec_cmd("swaybg -i ~/.config/hypr/images/zelda-and-link.jpg -m fill")
+  hl.exec_cmd("hyprpaper & waybar & swaync & hypridle")
   hl.exec_cmd("cliphist wipe")
   hl.exec_cmd("wl-paste --watch cliphist store")
 end)
