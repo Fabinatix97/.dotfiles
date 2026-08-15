bcc4fbc865ee7329bc4dd27bf881ee9e57099596
Fabinatix97 <fabian@fabi-online.de>
2026-08-15 20:50:55 +0200

    Resolve scaling issues

--- hypr/.config/hypr/modules/env.lua
+++ hypr/.config/hypr/modules/env.lua
@@ -1,3 +1,9 @@
 -- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+-- https://wiki.hypr.land/Configuring/Advanced-and-Cool/XWayland/
+-- force_zero_scaling leaves XWayland apps at 1x physical pixels on a
+-- scale-2 panel unless toolkits scale themselves.
+hl.env("GDK_SCALE", "2")
+hl.env("QT_SCALE_FACTOR", "2")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "0")
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
