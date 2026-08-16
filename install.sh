f50caa59d1fad6e4ba39611dc11824a448c3b782
Fabinatix97 <fabian@fabi-online.de>
2026-08-16 14:29:04 +0200

    Update nvm setup during installation

--- install.sh
+++ install.sh
@@ -104,6 +104,9 @@ systemctl --user enable hyprpolkitagent.service
 
 # Install node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 26
 
 npm install -g markdownlint-cli2 markdown-toc prettier tree-sitter-cli
