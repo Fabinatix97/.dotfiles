f6bb45ed55d147a4633b1711e3f60a196b29826b
Fabinatix97 <fabian@fabi-online.de>
2026-08-18 20:49:19 +0200

    Temporarily disable netrw_keepdir

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,7 +6,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_list_hide = [[^\.\.\=/\=$]]
-vim.g.netrw_keepdir = 0
+-- vim.g.netrw_keepdir = 0   -- Still not sure if I wanna keep this or not
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
