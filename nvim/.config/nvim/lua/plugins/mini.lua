b4f3fa07378274ba0ed74ec47043eb1cafcca53b
Fabinatix97 <fabian@fabi-online.de>
2026-08-23 11:39:50 +0200

    Remove unused format_on_save function - conform.lua owns formatting

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,16 +1,4 @@
 return {
-	format_on_save = function(bufnr)
-		-- You can specify filetypes to autoformat on save here:
-		local enabled_filetypes = {
-			-- lua = true,
-			-- python = true,
-		}
-		if enabled_filetypes[vim.bo[bufnr].filetype] then
-			return { timeout_ms = 500 }
-		else
-			return nil
-		end
-	end,
 	"nvim-mini/mini.nvim",
 	config = function()
 		require("mini.ai").setup({
