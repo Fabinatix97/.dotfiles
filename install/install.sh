89161c30862e7ab3db43817f575e4bbeac9add61
Fabinatix97 <fabian@fabi-online.de>
2026-08-29 08:05:10 +0200

    Make sure install scripts don't append to the stowed .bashrc

--- install/install.sh
+++ install/install.sh
@@ -109,8 +109,8 @@ source "$HOME/.bashrc"
 # Enable hyprpokitagent
 systemctl --user enable hyprpolkitagent.service
 
-# Install node version manager
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | bash
+# Install node version manager (PROFILE=/dev/null: do not append to the stowed .bashrc)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | PROFILE=/dev/null bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
@@ -124,9 +124,9 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Dark mode as default
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 
-# Install sdkman for managing jdks and sdks
+# Install sdkman for managing jdks and sdks (rcupdate=false: do not append to the stowed .bashrc)
 bash -c '
-  curl -s "https://get.sdkman.io" | bash
+  curl -s "https://get.sdkman.io?ci=true&rcupdate=false" | bash
   source "$HOME/.sdkman/bin/sdkman-init.sh"
   sdk install java 26-tem
   sdk install maven
