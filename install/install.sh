331a3226b3ece98f32911a4dcd2c35b41f4da0f3
Fabinatix97 <fabian@fabi-online.de>
2026-08-29 19:47:03 +0200

    Update nvm version

--- install/install.sh
+++ install/install.sh
@@ -139,7 +139,7 @@ source "$HOME/.bashrc"
 systemctl --user enable hyprpolkitagent.service
 
 # Install node version manager (PROFILE=/dev/null: do not append to the stowed .bashrc)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.6/install.sh | PROFILE=/dev/null bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | PROFILE=/dev/null bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
